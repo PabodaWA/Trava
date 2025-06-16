63c5078bbeb60d30242537d5ac6158037da77773
PabodaWA <wampaboda@gmail.com>
2025-06-16 18:34:37 +0530

    modify

---
+++ Index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body, html {
+  height: 100%;
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+.bg-image {
+  /* Full height */
+  height: 50%; 
+  
+  /* Center and scale the image nicely */
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+/* Images used */
+.img1 { background-image: url("PIC/1307256.jpg"); }
+.img2 { background-image: url("PIC/1307260.jpg"); }
+.img3 { background-image: url("PIC/3558149.jpg"); }
+.img4 { background-image: url("PIC/amanwella-beach-tangalle-luxury-hotel-sri-lanka (1).jpeg"); }
+.img5 { background-image: url("PIC/sri-lankan-leopard-76281.jpg"); }
+.img6 { background-image: url("PIC/elephant-rock.jpg"); }
+
+/* Position text in the middle of the page/image */
+.bg-text {
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0, 0.4); /* Black w/opacity/see-through */
+  color: white;
+  font-weight: bold;
+  font-size: 80px;
+  border: 10px solid #f1f1f1;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 2;
+  width: 1000px;
+  padding: 20px;
+  text-align: center;
+}
+</style>
+</head>
+<body>
+
+<div class="bg-image img1"></div>
+<div class="bg-image img2"></div>
+<div class="bg-image img3"></div>
+<div class="bg-image img4"></div>
+<div class="bg-image img5"></div>
+<div class="bg-image img6"></div>
+
+<div class="bg-text">Welcome to Trava <div>
+
+
+</body>
+</html>
